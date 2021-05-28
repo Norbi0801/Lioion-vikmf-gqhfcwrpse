6a565430056e1e59e9a6049844f3f910406410ab
polawskid <d.polawski@student.mini.pw.edu.pl>
2021-05-29 01:05:59 +0200

    New function to calculate value tags in semester

--- BoardGames_and_Arqade.R
+++ BoardGames_and_Arqade.R
@@ -1,7 +1,16 @@
-install.packages(lubridate)
 library(lubridate)
-Posts <- data.table(Posts_xml)
+library(data.table)
+library(dplyr)
+Posts_chess <- data.table(Posts_xml)
 
-Posts1 <- Posts %>%
+value_tags_semester <- function(Posts_xml){
+
+Posts1 <- Posts_xml %>%
   mutate(Semester = paste0(substring(year(CreationDate),1,4),"/0",semester(CreationDate)))
 
+y <- split(Posts1, by = c("Semester"))
+y <- lapply(y, FUN = function(x){
+  nrow(x)
+})
+
+}
