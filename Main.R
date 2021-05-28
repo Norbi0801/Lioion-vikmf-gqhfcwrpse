28d831919af1916c7294bf98b787f5aad3bf32a0
jmuszyn <72436360+jmuszyn@users.noreply.github.com>
2021-05-28 23:04:40 +0200

    Update Main.R

--- Main.R
+++ Main.R
@@ -46,7 +46,45 @@ zapytanie_1 <- function(Posts_xml){
   #ans1 <- ans[,
   #            .(best_tag = max(Tags_number)),
   #            by = c("CreationDate")]
-  #TODO Czy skupiamy si� na wyselekcjonowaniu miesi�cy
+  #TODO Czy skupiamy si? na wyselekcjonowaniu miesi?cy
 }
 x <- zapytanie_1(arqade.Posts)
 #plot(x$CreationDate, x$Tags_number)
+
+# 25 userów z największą ilością użytych tagów. 
+tags_users_comparision <- function(Posts_xml, Users_xml){
+  PostsDT1 <- data.table(Posts_xml)
+  PostsDT <- PostsDT1[, .(Id,
+                          PostTypeId,
+                          CreationDate,
+                          OwnerUserId,
+                          Tags)]
+  PostsDT <- PostsDT[PostTypeId == 1, ]
+  
+  PostsDT$Tags <- substr(PostsDT$Tags,
+                         2,
+                         nchar(PostsDT$Tags) - 1)
+  split <- strsplit(PostsDT$Tags, split = "><")
+  split1 <- data.frame(Id = rep(PostsDT$Id, sapply(split, length)), Tags = unlist(split))
+  PostsDT <- PostsDT[, Tags := NULL]
+  PostsDT <- merge(PostsDT, split1, by = "Id")
+  
+  UsersDT1 <- data.table(Users_xml)
+  UsersDT <- UsersDT1[, .(Id,
+                          DisplayName,
+                          Location,
+                          Difference = (UpVotes - DownVotes))]
+  UsersDT <- UsersDT[Id != -1, ]
+  
+  DT <- merge(PostsDT, UsersDT, by.x = "OwnerUserId", by.y = "Id")
+  
+  DT1 <- count(DT, OwnerUserId, wt = n_distinct(Tags), name = "Number_of_Tags")
+  
+  ans <- merge(UsersDT, DT1, by.x = "Id", by.y = "OwnerUserId")
+  ans <- ans[, Id := NULL]
+  ans <- ans[Difference >= 1000, ]
+  
+  ans <- ans[order(Number_of_Tags, decreasing = TRUE)]
+  ans <- ans[1:25, ]
+  return(ans)
+}
