fb89d8c4d262f44a39e8f99db17293979d1575a5
jmuszyn <72436360+jmuszyn@users.noreply.github.com>
2021-05-29 01:04:42 +0200

    Update Main.R

--- Main.R
+++ Main.R
@@ -92,3 +92,28 @@ tags_users_comparision <- function(Posts_xml, Users_xml){
   ans <- ans[1:25, ]
   return(ans)
 }
+
+### QUEENS GAMBIT ###
+
+PostsDT <- data.table(Posts_xml)
+
+Questions <- PostsDT[PostTypeId == 1, ]
+Questions_before <- Questions["2020-06-01" <= CreationDate & CreationDate <= "2020-10-31", ]
+Questions_after <- Questions[CreationDate >= "2020-11-01", ]
+Answers <- PostsDT[PostTypeId == 2, ]
+Answers_before <- Answers["2020-06-01" <= CreationDate & CreationDate <= "2020-10-31", ]
+Answers_after <- Answers[CreationDate >= "2020-11-01", ]
+
+numbers <- function(DT){
+  DT <- setDT(DT)[, CreationDate := format(as.Date(CreationDate), "%Y-%m") ]
+  DT <- DT[, .(Number = .N),
+           by = "CreationDate"]
+  return(DT)
+}
+
+Questions_before <- numbers(Questions_before)
+Questions_after <- numbers(Questions_after)
+Answers_before <- numbers(Answers_before)
+Answers_after <- numbers(Answers_after)
+
+###################
